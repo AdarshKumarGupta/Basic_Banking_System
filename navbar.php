698daba681ca7ed07895d5df2102177053e85796
AdarshKumarGupta <84294499+AdarshKumarGupta@users.noreply.github.com>
2021-05-17 05:00:17 +0530

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,29 @@
+<!-- navbar -->
+<nav class="navbar navbar-expand-md navbar-light bg-dark">
+    <button style="background-color: white;"><a href="index.php" class="navbar-brand" style="color: #0495B4;"><img src="img/logo.jpg"></a></button>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navabar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <button style="background-color: white;"><a href="index.php" class="nav-link" style="color: black ;"><b>Home</b></a></button>
+            </li>
+            <!--
+			<li class="nav-item">
+               <button style="background-color: white;"> <a href="createuser.php" class="nav-link" style="color: black ;"><b>Create User</b></a></button>
+            </li>
+			-->
+            <li class="nav-item">
+               <button style="background-color: white;"> <a href="transfermoney.php" class="nav-link" style="color: black ;"><b>Transfer Money</b></a></button>
+            </li>
+            <li class="nav-item">
+                <button style="background-color: white;"><a href="transactionhistory.php" class="nav-link" style="color: black ;"><b>Transaction History</b></a></button>
+            </li>
+
+        </ul>
+
+    </div>
+
+</nav>
